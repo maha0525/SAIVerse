503652915f894b0d8b57bbdb4d0ba26cdb2e88c7
R_N_A <33271130+RNA4219@users.noreply.github.com>
2026-02-22 19:12:20 +0900

    Fix runtime_llm helper imports for template formatting

--- sea/runtime_llm.py
+++ sea/runtime_llm.py
@@ -5,7 +5,9 @@ import logging
 import os
 import re
 from typing import Any, Callable, Dict, Optional
+
 from llm_clients.exceptions import LLMError
+from sea.runtime_utils import _format, _is_llm_streaming_enabled
 from saiverse.logging_config import log_sea_trace
 from sea.playbook_models import PlaybookSchema
 from saiverse.usage_tracker import get_usage_tracker
--- tests/test_runtime_utils_regression.py
+++ tests/test_runtime_utils_regression.py
@@ -1,4 +1,5 @@
 from sea import runtime
+from sea import runtime_llm
 from sea import runtime_utils
 
 
@@ -8,3 +9,8 @@ def test_runtime_exposes_shared_format_helper() -> None:
 
 def test_runtime_exposes_shared_streaming_helper() -> None:
     assert runtime._is_llm_streaming_enabled is runtime_utils._is_llm_streaming_enabled
+
+
+def test_runtime_llm_exposes_shared_helpers() -> None:
+    assert runtime_llm._format is runtime_utils._format
+    assert runtime_llm._is_llm_streaming_enabled is runtime_utils._is_llm_streaming_enabled
